ee8c56183d0e1a3dd1a2b1809d2a3f0a4329484f
RonnyRice <82329993+RonnyRice@users.noreply.github.com>
2022-01-26 14:56:27 +0000

    work

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -2,8 +2,38 @@ package ie.tudublin;
 
 public class Cat extends Animal
 {
+    private int numLives; 
+
     public Cat(String name)
     {
         super(name);
+        numLives = 9;
+        
+    }
+
+    public void setNumLives(int numLives)
+    {
+        this.numLives = numLives;
+    }
+
+    public int kill()
+    {
+        
+        this.setNumLives(this.numLives);
+
+        if (this.numLives > 0 )
+        {
+            System.out.println("ouch");
+            return this.numLives = this.numLives - 1;
+        
+        }
+
+        else
+        {
+            System.out.println("Dead");
+            return 0;
+
+        }
+
     }
 }
